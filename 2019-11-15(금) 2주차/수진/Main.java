22fc80fa683f5448cf8062f12bc2ad9c94c7eddb
kimsujin <32222724+anyin93@users.noreply.github.com>
2019-12-03 23:12:26 +0900

    차이를최대로

--- 2019-11-15(금) 2주차/수진/Main.java
+++ 2019-11-15(금) 2주차/수진/Main.java
@@ -1,63 +1,102 @@
-package 일이삼더하기;
+package 차이를최대로;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int N;
 	static int[] num;
-	static int count;
-	
+	static int[] num2;
+	static int[] num3;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		int T;
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 		
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		T=Integer.parseInt(br.readLine().trim());
-		num=new int[T];
 		
-		for(int t=0;t<T;t++) {
-			num[t]=Integer.parseInt(br.readLine().trim());
-			
+		N=Integer.parseInt(br.readLine().trim());
+		
+		num=new int[N];
+		num2=new int[N];
+		num3=new int[N];
+		
+		StringTokenizer st=new StringTokenizer(br.readLine().trim()," ");
+		for(int i=0;i<N;i++) {
+			num[i]=Integer.parseInt(st.nextToken());
 		}
 		
-		for(int t=0;t<T;t++) {
-			int sum=0;
-			count=0;
+		Arrays.parallelSort(num);
+
+		
+		int count=0;
+		int i,j,a,b;
+		
+		i=(N/2)-1;
+		j=N/2;
+		
+
+		
+		for(a=0, b=N-1;i>=0&&j<N;i--,j++,a++,b--) {
+				if(count%2==0) {
+					num2[i]=num[a];
+					num2[j]=num[b];
+					
+				}
+				else {
+					num2[i]=num[b];
+					num2[j]=num[a];
+				}
+				
 			
-			dfs(num[t],sum);
-			System.out.println(count);
-		}
-		 
-		
-		
-	}
-
-
-	private static void dfs(int num, int sum) {
-		
-		if(sum==num) {
-
 			count++;
-			return;
-			
-		//sum이 num값을 넘어가면 종료시킴	
-		}else if(sum>=num) {
-			return;
 		}
 		
 		
-		for(int i=1;i<=3;i++) {
-			sum+=i;
+		if(N%2!=0) {
+			num2[N-1]=num[a];
+			i=(N/2);
+			j=(N/2)+1;
+
+			count=0;
+			for(a=0, b=N-1;i>=0&&j<N;i--,j++,a++,b--) {
+//				System.out.println(num[a]+" and "+num[b]+i+"  "+j);
+					if(count%2==0) {
+						num3[i]=num[a];
+						num3[j]=num[b];
+						
+					}
+					else {
+						num3[i]=num[b];
+						num3[j]=num[a];
+					}
+					
+				
+				count++;
+			}
+			num3[0]=num[a];
+		}
 			
-			dfs(num,sum);
-			sum-=i;
+		
+
+//		for(int k=0;k<N;k++) {
+//			System.out.print(num2[k]+" ");
+//		}
+//		System.out.println();
+//		for(int k=0;k<N;k++) {
+//			System.out.print(num3[k]+" ");
+//		}
+//		System.out.println();
+		
+		int cha1=0;
+		int cha2=0;
+		for(int k=0;k<N-1;k++) {
+			cha1+=Math.abs(num2[k]-num2[k+1]);
+			cha2+=Math.abs(num3[k]-num3[k+1]);
+			
 		}
 		
-			
+		System.out.println(Math.max(cha1,cha2));
 	}
-
-
-	
-
 }
