9097cada27d824e2f4f477350a3efe2b21f70c21
kimsujin <32222724+anyin93@users.noreply.github.com>
2019-12-06 01:12:18 +0900

    토마토

--- 2019-11-29(금) 3주차/수진/Main.java
+++ 2019-11-29(금) 3주차/수진/Main.java
@@ -1,82 +1,113 @@
-package 숨바꼭질;
+package 토마토;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N,K;
-	static int count=0;
-	static Queue<Integer> q=new LinkedList<Integer>();
-	static int[] v;
 
+	static int N,M;
+	static int[][] tom;
+	static int[] dx= {-1,0,1,0};
+	static int[] dy= {0,1,0,-1};
+	static Queue<point> q=new LinkedList<point>();
+			
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		v=new int[100002];
 		
 		StringTokenizer st=new StringTokenizer(br.readLine().trim()," ");
+		
 		N=Integer.parseInt(st.nextToken());
-		K=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		tom=new int[M][N];
+		
+		
+		for(int i=0;i<M;i++) {
 			
-		q.add(N);
-		v[N]=1;
-		
-		bfs();
-		
-//		int ans=0;
-//		while(count>1) {
-//			count/=3;
-//			ans++;
-//		}
-		System.out.println(v[K]-1);
-	}
-
-	private static void bfs() {
-		
-		
-
-		while(!q.isEmpty()) {
-			//count++;
-
-			int num=q.poll();
-			if(num==K) break;
-			
-			for(int i=0;i<3;i++) {
-				int tmp=0;
-				switch(i) {
-					case 0:
-						tmp=num+1;
-						break;
-					case 1:
-						tmp=num-1;
-						break;
-					case 2:
-						tmp=num*2;
-						break;
-					default:
-						break;
-				}
-				if(tmp<0) continue;
-				if( tmp<=100000) {
-					if(v[tmp]==0) {
-						q.add(tmp);
-						v[tmp]=v[num]+1;
-					}
-					
-
+			st=new StringTokenizer(br.readLine().trim()," ");
+			for(int j=0;j<N;j++) {
+				tom[i][j]=Integer.parseInt(st.nextToken());
+				if(tom[i][j]==1) {
+					q.add(new point(i,j,1));
 				}
 			}
-			
-			
 		}
 		
+		
+		//토마토 익게하기
+		bfs();
+		//검사
+		
+		int max=Integer.MIN_VALUE;
+		
+		forout:
+		for(int i=0;i<M;i++) {
+			
+			for(int j=0;j<N;j++) {
+				if(tom[i][j]==0) {
+					max=-1;
+					break forout;
+				}
+				if(max<tom[i][j]) {
+					max=tom[i][j];
+					
+				}
+			}
+		}
+		
+		if(max==-1)System.out.println("-1");
+		else System.out.println(max-1);
 	}
 	
-	
-}	
+	private static void bfs() {
+		
+		while(!q.isEmpty()) {
+			point p=q.poll();
+			
+			
+			for(int i=0;i<4;i++) {
+				int nx=p.x+dx[i];
+				int ny=p.y+dy[i];
+				
+				if(nx<0||nx>=M||ny<0||ny>=N) {
+					continue;
+				}
+				
+				else if( tom[nx][ny]==0) {
+					tom[nx][ny]=p.count+1;
+					
+					q.add(new point(nx,ny,p.count+1));
+					
+					
+				}
+			}
+		}
+	}
+
+	public static class point{
+		public int x,y;
+		public int count;
+
+		public point(int x, int y,int count) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.count = count;
+		}
+
+		@Override
+		public String toString() {
+			return "point [x=" + x + ", y=" + y + ", count=" + count + "]";
+		}
+
+		
+
+		
+		
+	}
+
+}
